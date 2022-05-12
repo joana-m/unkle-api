a5953204060f3f823e29df4758588ba7caa9252e
Joana Moufat <joana.moufat@gmail.com>
2022-05-12 12:17:17 +0800

    init: routes index destroy for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       root to: 'pages#home'
-      # resources :contract, only: [ :index ]
+      resources :users, only: [ :index, :destroy ]
     end
   end
 end
