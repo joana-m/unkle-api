fe6ba03eaae03de77a47d68f93cc94d21a377a27
Joana Moufat <joana.moufat@gmail.com>
2022-05-12 10:46:48 +0800

    init: model Option created

---
+++ db/migrate/20220512024610_create_options.rb
@@ -0,0 +1,11 @@
+class CreateOptions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :options do |t|
+      t.string :type
+      t.string :description
+      t.string :identifiant
+
+      t.timestamps
+    end
+  end
+end
