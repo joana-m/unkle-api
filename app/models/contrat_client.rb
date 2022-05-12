30e032747d27b5646b08fc368aa36e310c48c78b
Joana Moufat <joana.moufat@gmail.com>
2022-05-12 11:26:20 +0800

    init: validations

--- app/models/contrat_client.rb
+++ app/models/contrat_client.rb
@@ -4,4 +4,19 @@ class ContratClient < ApplicationRecord
 
   has_many :contrat_options, through: :contrats
   has_many :options, through: :contrat_options
+
+  validates :date_de_debut, presence: true
+  validate :date_de_fin_ne_peut_etre_dans_le_passe, :date_de_fin_ne_peut_etre_avant_la_date_de_debut
+
+  def date_de_fin_ne_peut_etre_dans_le_passe
+    if !date_de_fin.nil? && date_de_fin < Date.today
+      errors.add(:date_de_fin, "La date de fin ne peut être dans le passé.")
+    end
+  end
+
+  def date_de_fin_ne_peut_etre_avant_la_date_de_debut
+    if !date_de_fin.nil? && date_de_fin < date_de_debut
+      errors.add(:date_de_fin, "La date de fin ne peut être avant la date de début.")
+    end
+  end
 end
--- app/models/option.rb
+++ app/models/option.rb
@@ -1,4 +1,7 @@
 class Option < ApplicationRecord
+  CATEGORIES = ["Tous risques", "Vols", "Incendie", "Cambriolage en semaine", "Cambriolage le week-end"]
   has_many :contrat_options
   has_many :contrats, through: :contrat_options
+
+  validates :categorie, inclusion: { in: CATEGORIES }, presence: true
 end
